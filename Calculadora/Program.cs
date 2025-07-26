2b63852c6e36f59f0ff75ee8a5cf9514a6354997
Luciano <ovejeromarcosluciano@gmail.com>
2025-07-26 17:24:52 -0300

    2do-commit

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Timers;
+using EspacioCalculadora;
+
+
+//Ejercicio 1
+Calculadora MiCalculadora = new Calculadora();
+
+Console.WriteLine("==========CALCULADORA============");
+
+Console.WriteLine("Ingrese un numero: ");
+string numero = Console.ReadLine();
+double num1;
+bool esNum1 = double.TryParse(numero, out num1);
+
+while (!esNum1)
+{
+    Console.WriteLine("El valor ingresado no es valido, por favor ingrese un numero valido");
+    numero = Console.ReadLine();
+    esNum1 = double.TryParse(numero, out num1);
+}
+MiCalculadora.Sumar(num1);
+Console.WriteLine($"Resultado: {MiCalculadora.Resultado}");
+
+Console.WriteLine("1 Suma\n2 Resta\n3 Multiplicar\n4 Dividir\nIngrese la operacion que desea realizar: ");
+numero = Console.ReadLine();
+int operacion;
+bool esnum1 = int.TryParse(numero, out operacion);
+while (operacion > 4 || !esnum1)
+{
+    Console.WriteLine("El numero ingresado no es una operacion disponible, porfavor ingrese de nuevo un numero valido");
+    numero = Console.ReadLine();
+    esnum1 = int.TryParse(numero, out operacion);
+}
+
+
